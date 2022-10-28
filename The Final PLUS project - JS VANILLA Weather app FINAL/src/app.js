f418c7fae97d11d4d8ffcf9ed3f98728598826a9
bibliobee <97406629+bibliobee@users.noreply.github.com>
2022-10-28 09:58:18 -0700

    Added the HTML structure

--- The Final PLUS project - JS VANILLA Weather app FINAL/src/app.js
+++ The Final PLUS project - JS VANILLA Weather app FINAL/src/app.js
@@ -1,1 +1,30 @@
-alert("Hello world");
+let submit = document.querySelector(`#submit`);
+submit.addEventListener(`click`, logCityName);
+
+function logCityName(event) {
+  event.preventDefault();
+  let city = getCityName();
+  console.log(city);
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = city;
+}
+
+function getCityName() {
+  event.preventDefault();
+  let city = document.querySelector(`#cityName`).value;
+  let apiKey = "b95f179627c8dd37f41e1be6e3250e19";
+  let url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${apiKey}&units=${metric}`;
+  axios.get(url).then(getCityName);
+}
+
+let apiKey = "b95f179627c8dd37f41e1be6e3250e19";
+let lat = document.querySelector(`response.data.lat`);
+let lon = document.querySelector(`response.data.lon`);
+let metric = document.querySelector(math.round(`response.data.temp`));
+let url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${apiKey}&units=${metric}`;
+axios.get(url).then(currentTemp);
+
+function currentTemp(response) {
+  let temperature = document.querySelector(`response.data.temp`);
+  h1.innerHTML(`temperature`);
+}
